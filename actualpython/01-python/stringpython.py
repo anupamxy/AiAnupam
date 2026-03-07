af5f6f56c8036aab058a05cb7cf632b5a5fcf2b9
anupamxy <anupam2k321@gmail.com>
2026-03-07 11:21:06 +0530

    string

---
+++ actualpython/01-python/stringpython.py
@@ -0,0 +1,8 @@
+name="Anupam Singh"
+print(f"first name:{name[:6]}")
+print(f"last name:{name[7:]}")
+print(f"reverse name:{name[::-1]}")
+encoded_name=name.encode("utf-8")
+decoded_name=encoded_name.decode("utf-8")
+print()
+
