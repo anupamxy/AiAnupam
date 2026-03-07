45f8ab4e0f8a20cd71f0f7424cb5deb52ae37057
anupamxy <anupam2k321@gmail.com>
2026-03-07 12:23:41 +0530

    changes

---
+++ actualpython/01-python/setspython.py
@@ -0,0 +1,11 @@
+a={"anupam","aman","rahul"}
+b={"anupam","ayush","amit"}
+all=a|b
+print(f"all:{all}")
+intersection=a&b
+print(f"intersection:{intersection}")
+difference=a-b
+print(f"difference:{difference}")
+symmetric_difference=a^b
+print(f"symmetric_difference:{symmetric_difference}")
+
