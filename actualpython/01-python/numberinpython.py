bc6a871c707efb67b526f7858dba8e2e7788c11d
anupamxy <anupam2k321@gmail.com>
2026-03-07 10:57:11 +0530

    numbers

---
+++ actualpython/01-python/numberinpython.py
@@ -0,0 +1,40 @@
+# Integer
+from AiAnupam import sys
+
+
+a=10
+b=5
+total=a+b
+print(f"Total:{total}")
+print(f"diff:{a-b}")
+print(f"division:{a/b}")
+print(f"floor division:{a//b}")
+print(f"modulas:{a%b}")
+print(f"power:{a**b}")
+print(f"negation:{-a}")
+
+
+#boolean
+is_true=True
+is_false=False
+n=5
+print(f"is_true:{is_true}")
+print(f"is_false:{is_false}")
+print(f"actions:{n + is_true}")
+
+
+#logation operations
+
+a=True
+b=False
+print(f"a:{a&b}")
+print(f"b:{a|b}")
+print(f"not a:{not a}")
+
+a=95.5
+b=4.555555
+print(f"sum:{a+b}")
+print(f"difference:{a-b}")
+print(f"product:{a*b}")
+print(f"quotient:{a/b}")
+print(f"information:{sys.float_info}")
