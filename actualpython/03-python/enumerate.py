46794e51f0dd615e131eb31b0931b273d7dd4f1f
anupamxy <anupam2k321@gmail.com>
2026-03-10 14:57:42 +0530

    changes

---
+++ actualpython/03-python/enumerate.py
@@ -0,0 +1,3 @@
+menu=["Green","Lemon","Spiced","mint"]
+for idx,item in enumerate(menu,start=1):
+    print(f"{idx}:{item} anupam")
---
+++ actualpython/03-python/ordersummary.py
@@ -0,0 +1,4 @@
+names=["Anupam","Aman","Sam"]
+bills=[1,2,3,4,5]
+for name,bills in zip(names,bills):
+    print(f"{name} paid {bills} rupees")
