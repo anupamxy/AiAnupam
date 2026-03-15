1a57988e2e600fb19007c60c4519211c8bdaa0b0
anupamxy <anupam2k321@gmail.com>
2026-03-15 11:20:05 +0530

    changes

---
+++ actualpython/pydantic/pydantic.py
@@ -0,0 +1,5 @@
+# pydantic provide data validation and settings management using python type annotations. It allows us to define data models using python classes and then validate the data against those models. It also provides a way to manage settings for our applications using environment variables or configuration files.
+# Data parsing and validation
+# API development
+# Settings management 
+# example of how to use pydantic
